01074fab8756d29fe8f09dcbd97ca3d35681f09f
Lee Siang Yin <siangyin916@gmail.com>
2022-02-09 16:48:48 +0800

    created products components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Products from "./components/Products/Products";
 
 const App = () => {
-	return <div>e-commerce</div>;
+	return <Products />;
 };
 
 export default App;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,1 +1,29 @@
+import Grid from "@material-ui/core/Grid";
+import Product from "./Product/Product";
 
+const product = [
+	{ id: 1, name: "shoes", description: "running shoes", price: "$5" },
+	{ id: 2, name: "macbook", description: "apple", price: "$5" },
+];
+
+import useStyles from "./styles";
+
+const Products = () => {
+	const classes = useStyles();
+
+	return (
+		<main>
+			<Grid container justify="center" spacing={4}>
+				{product.map((product) => {
+					return (
+						<Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+							<Product product={product} />
+						</Grid>
+					);
+				})}
+			</Grid>
+		</main>
+	);
+};
+
+export default Products;
